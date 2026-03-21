604ab78528cdfb7dc79da109275fa43a34a7f085
anujpanchal073-sudo <anujpanchal073@gmail.com>
2026-03-21 16:03:37 +0530

    Added program to find the length of a Linked List

---
+++ LinkedList/Length_of_LinkedList.java
@@ -0,0 +1,25 @@
+/*
+class Node{
+    int data;
+    Node next;
+    Node(int a){  data = a; next = null; }
+}*/
+
+public class Length_of_LinkedList {
+
+    //The function takes the head of a linked list as parameter and return the number of nodes in the linklist(Length of LinkedList)
+    public int getCount(Node head) {
+        // code here
+        int numberOfNodes = 0;
+        Node curr = head;
+        
+        //The Loop will run the exact number of times as the number of nodes present in the given LinkedList
+        //And each time the loop executes it increases th numberOfNodes variable by one
+        while(curr != null){
+            numberOfNodes += 1;
+            curr = curr.next;
+        }
+        
+        return numberOfNodes;
+    }
+}
